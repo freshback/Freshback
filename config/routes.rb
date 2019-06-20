76c6efdbc3ec38270ff9f17873e7d5a69d94e4b2
freshback <freshback1504@gmail.com>
2019-06-20 14:57:36 +0300

    Add Profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 	Rails.application.routes.draw do
-
   devise_for :users
   root 'keshbacks#index'
 
   resources :keshbacks
 	resources :comments, only: %i[create destroy]
+	resources :users,    only: %i[index show]
 end
