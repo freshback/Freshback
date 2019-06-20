0ce28d7ccf9f3014b49507e422deaae49fed2f27
freshback <freshback1504@gmail.com>
2019-06-20 23:38:54 +0300

    Add validates

--- app/models/keshback.rb
+++ app/models/keshback.rb
@@ -2,7 +2,14 @@ class Keshback < ApplicationRecord
   belongs_to :user
 
   has_many :comments, dependent: :destroy
-  
+
   mount_uploader :photo, PhotoUploader
+
+  validates :name,        presence: :true
+  validates :description, presence: :true
+  validates :link,        presence: :true
+  validates :count,       presence: :true
+  validates :photo,       presence: :true
+
   #serialize :photo, JSON
 end
