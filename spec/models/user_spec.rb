f3db128fefb78ad89416d581fdec7489f576c8bf
markelovv16 <markelovv1617@gmail.com>
2019-06-23 17:55:39 +0300

    Tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,37 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject{
+     build(:user)
+    }
+
+  describe "validation" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+    it "should not be valid without nickname" do
+       subject.nickname = nil
+       expect(subject).to_not be_valid
+    end
+
+    it "should not be valid without email" do
+       subject.email = nil
+       expect(subject).to_not be_valid
+    end
+
+    it "should not be valid without password" do
+       subject.password = nil
+       expect(subject).to_not be_valid
+    end
+    it { should validate_presence_of(:nickname) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+  end
+
+  context "associations" do
+    it { should have_many(:keshbacks).dependent(:destroy)}
+    it { should have_many(:comments).dependent(:destroy)}
+  end
 end
